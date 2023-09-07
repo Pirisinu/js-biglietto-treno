1b86b2daf6feb3d124b37d8757c0e0b2bc53422a
Pirisinu <palazzialessandro2@gmail.com>
2023-09-07 14:57:41 +0200

    [INIT]Repo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* 
+  CALCOLO DEL BIGLIETTO DEL TRENO
+  Chiedere al cliente il numero di chilometri che vorra percorrere e la sua età.
+  Moltiplicare il numero di chilometri scelti dal cliente al prezzo del biglietto fornito dal database(0.21 € al km).
+  Applicare al risultato uno sconto basato sull'età del nostro cliente:
+    -sconto del 20% per i minorenni
+    -sconto del 40% per gli over 65.
+*/
